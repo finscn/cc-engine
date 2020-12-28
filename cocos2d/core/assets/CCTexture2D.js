b65634b685cf1c30953b32b2dfb60d2a5bb8bb1c
finscn <finscn@gmail.com>
2020-12-28 23:05:44 +0800

    merge: Move Label packable setting out of Texture2D, fix wechat issue. #8038

--- cocos2d/core/assets/CCTexture2D.js
+++ cocos2d/core/assets/CCTexture2D.js
@@ -887,10 +887,6 @@ var Texture2D = cc.Class({
             this._packable = false;
             return;
         }
-
-        if (this._image && this._image instanceof HTMLCanvasElement) {
-            this._packable = true;
-        }
     },
 
     _getOpts() {
